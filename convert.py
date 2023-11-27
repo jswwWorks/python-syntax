f6ddf6ab929fdd829cc5067428ab518cbae9983f
Charles Chitty <charlescchitty@gmail.com>
2023-11-27 12:01:11 -0800

    Finished part 2 of Python-Syntax work

--- convert.py
+++ convert.py
@@ -1,7 +1,9 @@
+CONVERSION_FACTOR = 1.8
+
 def convert_temp(unit_in, unit_out, temp):
     """Convert fahrenheit <-> celsius and return results.
 
-    - unit_in: either "f" or "c" 
+    - unit_in: either "f" or "c"
     - unit-out: either "f" or "c"
     - temp: temperature (in f or c, depending on unit_in)
 
@@ -17,6 +19,18 @@ def convert_temp(unit_in, unit_out, temp):
 
     # YOUR CODE HERE
 
+    converted_temp = temp
+
+    if unit_in == "f" and unit_out == "c":
+        converted_temp = (temp - 32) * (1 / CONVERSION_FACTOR)
+    elif unit_in == "c" and unit_out == "f":
+        converted_temp = (temp * CONVERSION_FACTOR) + 32
+    elif not unit_in == "f" and not unit_in == "c":
+        return (f"Invalid input unit {unit_in}")
+    elif not unit_out == "f" and not unit_out == "c":
+        return (f"Invalid output unit {unit_out}")
+
+    return converted_temp
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
